78c9dc74098a923280bca0c7c74d327ec2d5a3f7
zhengxiaoqiang <zhengxiaoqiang@meituan.com>
2016-02-07 18:11:40 +0800

    1. add OMError to wrap all errors 2. refactor Task class. 3. load posts at launch.

--- app/src/main/java/com/frankzheng/app/omelette/task/Task.java
+++ app/src/main/java/com/frankzheng/app/omelette/task/Task.java
@@ -5,32 +5,61 @@ import java.util.WeakHashMap;
 /**
  * Created by zhengxiaoqiang on 16/2/4.
  */
-public class Task<T> {
+public abstract class Task<T> implements Runnable {
 
-    public interface TaskListener<T> {
-        void onSuccess(Task task, T data);
-        void onError(int code);
+    public abstract static class TaskListener<T> {
+        public void onSuccess(Task task, T data) {
+
+        }
+
+        public void onError(OMError error) {
+
+        }
+
+        public void onComplete(Task task) {
+
+        }
     }
 
-    protected WeakHashMap<TaskListener, Boolean> listeners = new WeakHashMap<>();
+    protected WeakHashMap<TaskListener<T>, Boolean> listeners = new WeakHashMap<>();
 
-    public void addTaskListener(TaskListener listener) {
+    public void addTaskListener(TaskListener<T> listener) {
         listeners.put(listener, true);
     }
 
-    public void removeTaskListener(TaskListener listener) {
+    public void removeTaskListener(TaskListener<T> listener) {
         listeners.remove(listener);
     }
 
+    @Override
+    public void run() {
+        try {
+            T data = doInBackground();
+            onSuccess(data);
+        } catch (Throwable t) {
+            onError(new OMError(t));
+        } finally {
+            onComplete();
+        }
+    }
+
+    abstract protected T doInBackground() throws Exception;
+
     protected void onSuccess(T data) {
-        for (TaskListener listener : listeners.keySet()) {
+        for (TaskListener<T> listener : listeners.keySet()) {
             listener.onSuccess(this, data);
         }
     }
 
-    protected void onError(int code) {
+    protected void onError(OMError error) {
         for (TaskListener listener : listeners.keySet()) {
-            listener.onError(code);
+            listener.onError(error);
+        }
+    }
+
+    protected void onComplete() {
+        for (TaskListener listener : listeners.keySet()) {
+            listener.onComplete(this);
         }
     }
 
