3793ae7883b612e48e393da328970eb6ca673300
zhengxiaoqiang <zhengxiaoqiang@meituan.com>
2016-03-16 18:29:00 +0800

    show pictures

--- app/src/main/java/com/frankzheng/app/omelette/ui/pictures/PicturesAdapter.java
+++ app/src/main/java/com/frankzheng/app/omelette/ui/pictures/PicturesAdapter.java
@@ -1,9 +1,13 @@
 package com.frankzheng.app.omelette.ui.pictures;
 
 import android.content.Context;
+import android.net.Uri;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
+import com.facebook.drawee.view.SimpleDraweeView;
 import com.frankzheng.app.omelette.R;
 import com.frankzheng.app.omelette.bean.Picture;
 import com.frankzheng.app.omelette.ui.BaseAdapter;
@@ -16,6 +20,7 @@ import butterknife.ButterKnife;
  * Created by zhengxiaoqiang on 16/3/14.
  */
 public class PicturesAdapter extends BaseAdapter<Picture> {
+    private static final String TAG = PicturesAdapter.class.getSimpleName();
 
     public PicturesAdapter(Context context) {
         super(context);
@@ -36,6 +41,8 @@ public class PicturesAdapter extends BaseAdapter<Picture> {
         TextView tv_author;
         @Bind(R.id.tv_title)
         TextView tv_title;
+        @Bind(R.id.iv_pic)
+        SimpleDraweeView iv_pic;
 
         public ViewHolder(View view) {
             ButterKnife.bind(this, view);
@@ -43,8 +50,11 @@ public class PicturesAdapter extends BaseAdapter<Picture> {
 
         @Override
         public void setData(Picture data) {
-            tv_author.setText(data.author);
+            tv_author.setText(TextUtils.isEmpty(data.author) ? "Anonymous" : data.author);
             tv_title.setText(data.title);
+            Uri uri = Uri.parse(data.picURL);
+            Log.i(TAG, uri.toString());
+            iv_pic.setImageURI(uri);
         }
     }
 
